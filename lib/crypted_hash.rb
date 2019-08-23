22a817a726ee32898a6693bc4ce8e67bc7ba810a
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2019-08-23 18:42:24 +0300

    Make CryptedHash work as Hash
    Also we can serialize it and save to a file and restore it from the
    file.
    
    Co-authored-by: Artem Starovoitov <epfeen@gmail.com>
    Co-authored-by: Maxim Spaseonav <maxrikal@gmail.com>
    Co-authored-by: Ilya Vasilevich <inudad@yandex.ru>
    Co-authored-by: Alexey Varvashevich <Amix047@gmail.com>
    Co-authored-by: Ivan Barysiuk <Jdjjad89coq5ven5uqoweOOvqw3n@gmail.com>
    Co-authored-by: Alex Samets <aliaksandr.samets@gmail.com>
    Co-authored-by: Margarita Omelchuk <margosha20617@gmail.com>
    Co-authored-by: Sokurenko Mikhail <tedpoolioo@gmail.com>
    Co-authored-by: Rudy Anton <midda7@mail.ru>
    Co-authored-by: Alexander Baranouski <Fr08ty@gmail.com>
    Co-authored-by: Julia Lugina <lugina.yulya@mail.ru>
    Co-authored-by: Andrew Berendeev <soligorsk162@gmail.com>
    Co-authored-by: Kirill Loktevich <lokkirill@gmail.com>

--- lib/crypted_hash.rb
+++ lib/crypted_hash.rb
@@ -1,2 +1,27 @@
 class CryptedHash
+  attr_reader :key, :hash
+  def initialize(key:, default_hash: {})
+    @key = key
+    @hash = default_hash
+  end
+
+  def [](key)
+    hash[key]
+  end
+
+  def []=(key, value)
+    hash[key] = value
+  end
+
+  def save!(path)
+    file = File.open(path, 'w+')
+    file.puts(Marshal.dump(hash))
+    file.close
+  end
+
+  def self.load(path, key)
+    file = File.read(path)
+    hash = Marshal.load(file)
+    self.new(key: key, default_hash: hash)
+  end
 end
--- spec/crypted_hash_spec.rb
+++ spec/crypted_hash_spec.rb
@@ -1,11 +1,43 @@
 require "spec_helper"
+require 'fileutils'
 
 RSpec.describe CryptedHash do
   it "can be instantiated" do
-    expect(CryptedHash.new).to be_instance_of(CryptedHash)
+    expect(CryptedHash.new(key: "test")).to be_instance_of(CryptedHash)
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "stores the key" do
+    hash = CryptedHash.new(key: "test")
+
+    expect(hash.key).to eq('test')
+  end
+
+  it 'saves values' do
+    hash = CryptedHash.new(key: 'test')
+
+    hash[:vk] = 'vkcom'
+
+    expect(hash[:vk]).to eq('vkcom')
+  end
+
+  it 'saves file' do
+    hash = CryptedHash.new(key: 'test')
+
+    FileUtils.rm('./tmp/test')
+    hash.save!('./tmp/test')
+
+    expect(File.exists?('./tmp/test')).to eq(true)
+  end
+
+  it 'loads file' do
+    hash = CryptedHash.new(key: 'test')
+    hash[:vkontakte] = 'vkcom'
+
+    FileUtils.rm('./tmp/test')
+    hash.save!('./tmp/test')
+    crypted_hash = CryptedHash.load('./tmp/test', 'test')
+
+    expect(crypted_hash[:vkontakte]).to eq('vkcom')
+    expect(crypted_hash).to be_instance_of(CryptedHash)
   end
 end
